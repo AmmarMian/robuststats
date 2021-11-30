153a545821f18f2b47b3f725898aab1dc469729c
Ammar Mian <ammar.mian@univ-savoie.fr>
2021-11-30 10:51:46 +0100

    Added some imports in the init

---
+++ robuststats/__init__.py
@@ -0,0 +1,15 @@
+import robuststats.detection
+import robuststats.estimation.base
+import robuststats.estimation.elliptical
+import robuststats.estimation.structured
+import robuststats.learning
+import robuststats.models.mappings
+import robuststats.models.manifolds
+import robuststats.models.probability
+import robuststats.models.online
+import robuststats.utils.checking
+import robuststats.utils.generation_data
+import robuststats.utils.linalg
+import robuststats.utils.matrix_operators
+import robuststats.utils.montecarlo
+import robuststats.utils.verbose
